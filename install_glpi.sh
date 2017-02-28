532434171802c4a665e8302fe54335e4a17d78f7
TheSkyEye <mailforgithubtheskyeye@yopmail.com>
2017-02-28 16:13:44 +0100

    Update install_glpi.sh

--- install_glpi.sh
+++ install_glpi.sh
@@ -3,10 +3,11 @@ apt-get install -y apache2 php5 php5-imap php5-ldap php5-curl php5-mysql mysql-s
 wget --no-check-certificate https://github.com/glpi-project/glpi/releases/download/9.1.2/glpi-9.1.2.tgz
 tar xvf glpi-9.1.2.tgz
 mv glpi /var/www/
+chown -R www-data:www-data glpi/
 chmod -R 777 /var/www/glpi/
 touch /etc/apache2/sites-available/glpi
 echo '<VirtualHost *:80>
-  ServerName glpi.domaine.fr
+  ServerName localhost
   DocumentRoot /var/www/glpi/
  <Directory /var/www/glpi>
         AllowOverride All
@@ -16,7 +17,6 @@ echo '<VirtualHost *:80>
  </Directory>
 </VirtualHost>' >> /etc/apache2/sites-available/glpi
 a2ensite glpi
-chown -R www-data:www-data /var/www/glpi
 /etc/init.d/apache2 reload
 #mysql -u root -p
 #GRANT all privileges ON glpi.* TO glpi@localhost IDENTIFIED BY 'glpi';
