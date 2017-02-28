7d48020ddb40c08fb6ee180e24284d343fd27e49
TheSkyEye <mailforgithubtheskyeye@yopmail.com>
2017-02-28 16:57:54 +0100

    Update install_glpi.sh

--- install_glpi.sh
+++ install_glpi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get install -y apache2 php5 php5-imap php5-ldap php5-curl php5-mysql mysql-server mysql-client
+apt-get install -y apache2 php5 libapache2-mod-php5 php5-mysql php5-gdphp5 php5-imap php5-ldap php5-curl mysql-server mysql-client
 wget --no-check-certificate https://github.com/glpi-project/glpi/releases/download/9.1.2/glpi-9.1.2.tgz
 tar xvf glpi-9.1.2.tgz
 mv glpi /var/www/
