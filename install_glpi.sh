9e39f0c996111f468eb362eab97e6f809f180b4b
TheSkyEye <mailforgithubtheskyeye@yopmail.com>
2017-04-02 02:49:51 +0200

    Update install_glpi.sh

--- install_glpi.sh
+++ install_glpi.sh
@@ -2,7 +2,6 @@
 apt-get install -y apache2 php5 libapache2-mod-php5 php5-mysql php5-gdphp5 php5-imap php5-ldap php5-curl mysql-server mysql-client
 wget --no-check-certificate https://github.com/glpi-project/glpi/releases/download/9.1.2/glpi-9.1.2.tgz
 tar xvf glpi-9.1.2.tgz
-exit 0
 mv glpi /var/www/
 chown -R www-data:www-data glpi/
 chmod -R 777 /var/www/glpi/
@@ -19,6 +18,13 @@ echo '<VirtualHost *:80>
 </VirtualHost>' >> /etc/apache2/sites-available/glpi
 a2ensite glpi
 /etc/init.d/apache2 reload
-#mysql -u root -p
-#GRANT all privileges ON glpi.* TO glpi@localhost IDENTIFIED BY 'glpi';
-#glpi/glpi for the administrator account
+
+read -p "Enter your MySQL root password: " rootpass
+read -p "Database name: " dbname
+read -p "Database username: " dbuser
+read -p "Enter a password for user $dbuser: " userpass
+echo "CREATE DATABASE $dbname;" | mysql -u root -p$rootpass
+echo "CREATE USER '$dbuser'@'localhost' IDENTIFIED BY '$userpass';" | mysql -u root -p$rootpass
+echo "GRANT ALL PRIVILEGES ON $dbname.* TO '$dbuser'@'localhost';" | mysql -u root -p$rootpass
+echo "FLUSH PRIVILEGES;" | mysql -u root -p$rootpass
+echo "New MySQL database is successfully created"
