94847bbc8dada0f78b4ad355d661993be2f4e827
TheSkyEye <mailforgithubtheskyeye@yopmail.com>
2017-04-02 03:48:08 +0200

    Update install_glpi.sh

--- install_glpi.sh
+++ install_glpi.sh
@@ -32,6 +32,6 @@ echo "New MySQL database is successfully created"
 wget --no-check-certificate https://forge.glpi-project.org/attachments/download/2179/GLPI-dashboard_plugin-0.8.2.tar.gz
 tar xzf GLPI-dashboard_plugin-0.8.2.tar.gz
 mv dashboard/ /var/www/html/glpi/plugins/
-wget --no-check-certificate https://github-cloud.s3.amazonaws.com/releases/39893877/82383e1a-03e7-11e7-8b54-c8a7f9f2a8b1.gz?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAISTNZFOVBIJMK3TQ%2F20170402%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20170402T012840Z&X-Amz-Expires=300&X-Amz-Signature=e3b7f1b059c56f10848675c628bbb6237fcfb3e4c695c1d64ba607cf2743e031&X-Amz-SignedHeaders=host&actor_id=22552177&response-content-disposition=attachment%3B%20filename%3Dglpi-ocsinventoryng-1.3.3.tar.gz&response-content-type=application%2Foctet-stream
+wget --no-check-certificate https://github.com/pluginsGLPI/ocsinventoryng/releases/download/1.3.3/glpi-ocsinventoryng-1.3.3.tar.gz
 tar xzf glpi-ocsinventoryng-1.3.3.tar.gz
 mv ocsinventoryng/ /var/www/html/glpi/plugins/
